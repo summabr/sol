0c30128a51f561f8ace33db7514e673254063d2f
einar <einar@summa.com.br>
2014-02-28 06:38:53 -0300

    JUnit

--- src/test/java/br/com/summa/sol/data/AbstractTopNTest.java
+++ src/test/java/br/com/summa/sol/data/AbstractTopNTest.java
@@ -8,33 +8,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class AbstractTopNTest {
-    protected void validate(int n, List<Integer> input, List<Integer> expected) {
-        validate(new ListTopN<Integer>(n), input, expected);
-        validate(new TreeTopN<Integer>(n), input, expected);
-    }
 
-    protected List<Integer> validate(TopN<Integer> topN, List<Integer> input,
-            List<Integer> expected) {
-        assertTrue(topN.isEmpty());
-        topN.addAll(input);
-        List<Integer> result = new ArrayList<Integer>(topN);
-        assertEquals(expected, result);
-        assertEquals(expected.size(), result.size());
-        assertEquals(expected.size(), topN.size());
-        assertFalse(topN.isEmpty());
-        return result;
-    }
+	private void validateContent(TopN<Integer> topN) {
+		Object[] objArray = topN.toArray();
+		Integer[] intArray = topN.toArray(new Integer[topN.size()]);
+		List<Integer> result = new ArrayList<Integer>(topN);
+		int i = 0;
+		for (Integer elem : topN) {
+			assertTrue(elem == objArray[i]);
+			assertTrue(elem == intArray[i]);
+			assertTrue(elem == result.get(i++));
+		}
+		assertEquals(topN.size(), i);
+		assertEquals(objArray.length, i);
+		assertEquals(intArray.length, i);
+		assertEquals(result.size(), i);
+	}
 
-    protected void validateRepetitionOrder(int n, List<Integer> input, List<Integer> expected) {
-        validateRepetitionOrder(new ListTopN<Integer>(n), input, expected);
-        validateRepetitionOrder(new TreeTopN<Integer>(n), input, expected);
-    }
+	protected void validate(int n, List<Integer> input, List<Integer> expected) {
+		validate(new ListTopN<Integer>(n), input, expected);
+		validate(new TreeTopN<Integer>(n), input, expected);
+	}
 
-    protected void validateRepetitionOrder(TopN<Integer> topN, List<Integer> input,
-            List<Integer> expected) {
-        List<Integer> result = validate(topN, input, expected);
-        for (int i=0; i < result.size(); i++) {
-            assertTrue(expected.get(i) == result.get(i));
-        }
-    }
+	protected List<Integer> validate(TopN<Integer> topN, List<Integer> input,
+			List<Integer> expected) {
+		assertTrue(topN.isEmpty());
+		topN.addAll(input);
+		List<Integer> result = new ArrayList<Integer>(topN);
+		assertEquals(expected, result);
+		assertEquals(expected.size(), result.size());
+		assertEquals(expected.size(), topN.size());
+		assertFalse(topN.isEmpty());
+		validateContent(topN);
+		return result;
+	}
+
+	protected void validateRepetitionOrder(int n, List<Integer> input, List<Integer> expected) {
+		validateRepetitionOrder(new ListTopN<Integer>(n), input, expected);
+		validateRepetitionOrder(new TreeTopN<Integer>(n), input, expected);
+	}
+
+	protected void validateRepetitionOrder(TopN<Integer> topN, List<Integer> input,
+			List<Integer> expected) {
+		List<Integer> result = validate(topN, input, expected);
+		for (int i=0; i < result.size(); i++) {
+			assertTrue(expected.get(i) == result.get(i));
+		}
+		validateContent(topN);
+	}
 }
