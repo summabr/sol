5ab17866bc60ac0fef0f1e66188c86868d4128e6
einar-saukas <einar@summa.com.br>
2015-02-10 11:29:50 -0200

    Additional functionality

---
+++ src/main/java/br/com/summa/sol/util/Reflections.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright 2012 by Summa Technologies do Brasil.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package br.com.summa.sol.util;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Convenience class for working with Java reflection.
+ *
+ * @author Einar Saukas
+ */
+public final class Reflections {
+
+    /**
+     * Prevents instantiation
+     */
+    private Reflections() {
+    }
+
+    /**
+     * Returns a list of all fields declared by the specified class or interface, or inherired from its super
+     * classes. However fields inherired from "super interfaces" are ignored.
+     *
+     * @param type Specified class or interface
+     * @return list of all fields declared by the specified class or interface, or inherired from its super
+     * classes
+     * @throws SecurityException if security manager denied access to field, or class loader didn't match
+     */
+    public static List<Field> getAllFields(Class<?> type) throws SecurityException {
+        List<Field> fields = new ArrayList<Field>();
+        for (Class<?> t = type; t != null && t != Object.class; t = t.getSuperclass()) {
+            fields.addAll(Arrays.asList(t.getDeclaredFields()));
+        }
+        return fields;
+    }
+
+    /**
+     * Returns the field declared by the specified class or interface, or inherired from its super
+     * classes, corresponding to the specified simple field name. Fields inherired from "super interfaces"
+     * are ignored.
+     *
+     * @param type Specified class or interface
+     * @param name Simple field name
+     * @return Field declared by the specified class or interface, or inherired from its super
+     * classes, corresponding to the specified simple field name
+     * @throws NoSuchFieldException if a field with the specified name is not found
+     * @throws SecurityException if security manager denied access to field, or class loader didn't match
+     */
+    public static Field getAnyField(Class<?> type, String name)
+            throws NoSuchFieldException, SecurityException {
+        try {
+            return type.getDeclaredField(name);
+        } catch (NoSuchFieldException e1) {
+            for (Class<?> t = type.getSuperclass(); t != null && t != Object.class; t = t.getSuperclass()) {
+                try {
+                    return t.getDeclaredField(name);
+                } catch (NoSuchFieldException e2) {
+                }
+            }
+            throw e1;
+        }
+    }
+}
